76db2a8d44e0d5a2404449c8aeff968eff10a8ad
Leonardo Malcotti <leo@Leonardos-Air.lan>
2024-07-20 15:33:00 +0200

    added controller

---
+++ src/main/java/com/sittingspot/querydatalayer/DTO/QueryInDTO.java
@@ -0,0 +1,13 @@
+package com.sittingspot.querydatalayer.DTO;
+
+import com.sittingspot.querydatalayer.models.Area;
+import com.sittingspot.querydatalayer.models.Tag;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * DTO for {@link com.sittingspot.querydatalayer.models.Query}
+ */
+public record QueryInDTO(Area area, List<Tag> tags, List<String> labels) implements Serializable {
+}
---
+++ src/main/java/com/sittingspot/querydatalayer/controller/QueryDataLayerController.java
@@ -0,0 +1,34 @@
+package com.sittingspot.querydatalayer.controller;
+
+import com.sittingspot.querydatalayer.DTO.QueryInDTO;
+import com.sittingspot.querydatalayer.models.Area;
+import com.sittingspot.querydatalayer.models.Query;
+import com.sittingspot.querydatalayer.models.QueryResult;
+import com.sittingspot.querydatalayer.models.Tag;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController("/query-dl/api/v1")
+public class QueryDataLayerController {
+
+    @GetMapping("/")
+    public List<Query> getQueries(@RequestParam("queryId") UUID queryId,
+                                  @RequestParam("location") Area location,
+                                  @RequestParam("tags") List<Tag> tags,
+                                  @RequestParam("labels") List<String> labels){
+
+        return List.of();
+    }
+
+    @PostMapping("/")
+    public UUID createQuery(@RequestBody QueryInDTO query) {
+
+        return UUID.randomUUID();
+    }
+
+    @PutMapping("/{Id}")
+    public void updateQuery(@PathVariable UUID Id, @RequestBody List<QueryResult> query) {}
+}
