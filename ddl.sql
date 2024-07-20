b16bb0783327e5a8dccf18ed201f7ed208027c10
Leonardo Malcotti <leo@Leonardos-Air.lan>
2024-07-20 15:01:09 +0200

    updated ddl file

--- ddl.sql
+++ ddl.sql
@@ -1,5 +1,36 @@
 
-create table Query (
-    query_id uuid
-)
+CREATE TABLE labels
+(
+    id     UUID NOT NULL,
+    labels VARCHAR(255)
+);
 
+CREATE TABLE query
+(
+    id   UUID  NOT NULL,
+    area JSONB NOT NULL,
+    CONSTRAINT pk_query PRIMARY KEY (id)
+);
+
+CREATE TABLE results
+(
+    id       UUID NOT NULL,
+    spot_id  UUID,
+    location JSON
+);
+
+CREATE TABLE tags
+(
+    id    UUID NOT NULL,
+    key   OID,
+    value OID
+);
+
+ALTER TABLE labels
+    ADD CONSTRAINT fk_labels_on_query FOREIGN KEY (id) REFERENCES query (id);
+
+ALTER TABLE results
+    ADD CONSTRAINT fk_results_on_query FOREIGN KEY (id) REFERENCES query (id);
+
+ALTER TABLE tags
+    ADD CONSTRAINT fk_tags_on_query FOREIGN KEY (id) REFERENCES query (id);
